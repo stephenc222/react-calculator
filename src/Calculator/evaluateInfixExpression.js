ae58c700eb761ac5bbe32a368a6fc4237e342d71
Stephen Collins <stephenc211@gmail.com>
2018-12-08 18:38:49 -0500

    updated package.json to appease vim, wip on UI state

---
+++ src/Calculator/evaluateInfixExpression.js
@@ -0,0 +1,86 @@
+const infixToPostfix = expression => {
+    const output = [];
+    const operatorStack = [];
+    const operators = '*/+-';
+    const count = expression.length;
+    const precendence = {
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2
+    };
+    
+    for (let i = 0; i < count; i += 1) {
+        const token = expression[i];
+      
+        if (operators.includes(token)) {
+            if (precendence[operatorStack[operatorStack.length - 1]] > precendence[token]) {
+                while (operatorStack.length) {
+                    output.push(operatorStack.pop());
+                }
+            }
+            operatorStack.push(token);
+        } else {
+            output.push(token);
+        }
+    }
+    
+    while (operatorStack.length) {
+        output.push(operatorStack.pop());
+    }
+    
+    return output;
+};
+
+const evaluatePostfixExpression = expression => {
+    const stack = [];
+    const count = expression.length;
+    const operators = '*/+-';
+    
+    let result = 0;
+    
+    const op = {
+        '+': (l, r) => +l + +r,
+        '-': (l, r) => +l - +r,
+        '*': (l, r) => +l * +r,
+        '/': (l, r) => +l / +r
+    };
+    
+    for (let i = 0; i < count; i += 1) {
+        const token = expression[i];
+        
+        if (operators.includes(token)) {
+            if (stack.length >= 2) {
+                const right = stack.pop();
+                const left = stack.pop();
+                const r = op[token](left, right);
+                console.log(`${left} ${token} ${right} = ${r}`);
+                stack.push(r);
+            } else {
+                throw new Error('Invalid Expression');
+            }
+        } else {
+            stack.push(token);
+        }
+    }
+    
+    if (stack.length === 1) {
+        result = stack.pop();
+    } else {
+        throw new Error('Invalid Expression');
+    }
+    
+    return result;
+};
+
+const evaluateInfixExpression = expression => {
+    const postfixExpression = infixToPostfix(expression);
+    return evaluatePostfixExpression(postfixExpression);
+};
+
+export default evaluateInfixExpression
+// 3 + 2 * 4 - 8 / 4 = 9
+//evaluateInfixExpression([ '3', '+', '2', '*', '4', '-', '8', '/', '4' ]);
+
+// evaluateInfixExpression(binaryExpressionToDecimalExpression(['11011', '+', '1000']));
+
--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react'
-import Button from '../Button'
-import Display from '../Display'
+import Button from './Button'
+import Display from './Display'
+import evaluateInfixExpression from './evaluateInfixExpression'
 
+// constants of the calculator keys
 const numKeys = [0,1,2,3,4,5,6,7,8,9]
-const opKeys = ['+', '-', 'x', '/', '=']
+const opKeys = ['+', '-', '*', '/', '=']
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props)
     this.state = {
       calcDisplay: '',
-      firstOp: null,
-      secondOp: null,
-      operation: ''
+      currentToken: '',
+      prevToken: '',
+      infixExpression: []
     }
   }
 
   onClick = (event) => {
-    console.warn('clicked', { name: event.target.name, value: event.target.value })
+    let { currentToken, prevToken } = this.state
+    const infixExpression = this.state.infixExpression.slice()
     const value = event.target.value
-    if (opKeys.indexOf(value) > -1) {
-      console.log('is opKey', {value})
-    } else {
-      console.log('is numKey', {value})
+    if (opKeys.includes(value) && value !== '=') {
+      infixExpression.push(currentToken)
+      prevToken = currentToken
+      currentToken = value
+      infixExpression.push(currentToken)
+    } else if (numKeys.includes(+value)){
+      currentToken += value
+      currentToken = currentToken.replace(/[+-/*]/, '')
     }
-    this.setState({calcDisplay: event.target.value})
+    if (infixExpression.length && value === '=') {
+      infixExpression.push(currentToken)
+      return this.setState({calcDisplay: evaluateInfixExpression(infixExpression), infixExpression: [], currentToken: '', prevToken: ''})
+    }
+    const cleanedToken = currentToken.replace(/[+-/*]/, '')
+    this.setState({
+      infixExpression,
+      currentToken,
+      prevToken,
+      calcDisplay: cleanedToken.length ? cleanedToken : prevToken
+    })
   }
 
   render() {
